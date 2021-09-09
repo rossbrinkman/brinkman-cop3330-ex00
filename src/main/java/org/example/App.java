7e274a6c64ac1ed06251ca8c52fcc99d6edca584
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-08 22:34:31 -0400

    00 finished

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Ross Brinkman
  */
-public class App 
+
+import java.io.InputStreamReader;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "What is your name? " );
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput = scanner.nextLine();
+
+        System.out.println( "Hello, " + userInput + ", nice to meet you!" );
+
+        scanner.close();
     }
 }
