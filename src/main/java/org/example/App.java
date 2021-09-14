61e4ca62ee309f5f41f4c5252a1920fbf02eb8ed
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-13 22:19:16 -0400

    1 finished

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,10 @@
 package org.example;
-
 import java.util.Scanner;
 
 /*
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 Ross Brinkman
  */
-
-import java.io.InputStreamReader;
 
 public class App
 {
